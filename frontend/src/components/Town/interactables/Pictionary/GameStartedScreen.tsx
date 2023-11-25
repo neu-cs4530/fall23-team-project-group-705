e36ccaa78ee76e7f767cce132a5db7fb078507c9
Oliver Baer-Benson <opbb83@gmail.com>
2023-11-24 20:51:10 -0600

    Made observers unable to interact

--- frontend/src/components/Town/interactables/Pictionary/GameStartedScreen.tsx
+++ frontend/src/components/Town/interactables/Pictionary/GameStartedScreen.tsx
@@ -3,15 +3,12 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 import EndGameScore from "./EndGameScore";
 import PictionaryAreaController from "../../../../classes/interactable/PictionaryAreaController";
 import { useEffect, useState } from "react";
-import { GameStatus } from "../../../../types/CoveyTownSocket";
-import PlayerController from "../../../../classes/PlayerController";
 
 function GameStartedScreen(props: {gameAreaController: PictionaryAreaController}): JSX.Element {
   const gameAreaController: PictionaryAreaController = props.gameAreaController;
 
   const [isPlayer, setIsPlayer] = useState<boolean>(gameAreaController.isPlayer);
-  const [gameStatus, setGameStatus] = useState<GameStatus>(gameAreaController.status || 'WAITING_TO_START');
-  const [drawer, setDrawer] = useState<PlayerController | undefined>(gameAreaController.drawer);
+  const [isOurTurn, setIsOurTurn] = useState<boolean>(gameAreaController.isOurTurn);
   const [currentWord, setCurrentWord] = useState<string>(gameAreaController.currentWord);
   const [betweenTurns, setBetweenTurns] = useState(gameAreaController.betweenTurns);
   const [timer, setTimer] = useState(gameAreaController.timer);
@@ -27,78 +24,89 @@ function GameStartedScreen(props: {gameAreaController: PictionaryAreaController}
 
   useEffect(() => {
       setIsPlayer(gameAreaController.isPlayer);
-      setGameStatus(gameAreaController.status || 'WAITING_TO_START');
-      setDrawer(gameAreaController.drawer);
+      setIsOurTurn(gameAreaController.isOurTurn);
       setCurrentWord(gameAreaController.currentWord);
       setTimer(gameAreaController.timer);
       setBetweenTurns(gameAreaController.betweenTurns);
     }, [
       gameAreaController.isPlayer,
-      gameAreaController.status,
-      gameAreaController.drawer,
+      gameAreaController.isOurTurn,
       gameAreaController.currentWord,
       gameAreaController.timer,
       gameAreaController.betweenTurns,
     ]);
 
+    const currentWordDisplay = 
+      <>
+        <Heading as='h4' size='md'>{betweenTurns ? 'The word was:' : 'Your word:'}</Heading>
+        {currentWord}
+      </>
+
   return (
     <HStack h={'2xl'} w={['sm', '2xl', '6xl']} alignItems='top' margin={2}>
         <Excalidraw />
         <VStack width={250} spacing='12' paddingTop={4}>
-        <div>
           <Heading as='h4' size='md'>
             {
               betweenTurns
               ? `${intermissionLength - timer} seconds until next turn.`
-              : `${turnLength - timer} seconds left to ${gameAreaController.isOurTurn ? 'draw' : 'guess'}.`
+              : `${turnLength - timer} seconds left to ${isOurTurn ? 'draw' : 'guess'}.`
             }
           </Heading>
-        </div>
         {
-          gameAreaController.isOurTurn || betweenTurns
-          ? 
-          <>
-            <Heading as='h4' size='md'>{betweenTurns ? 'The word was:' : 'Your word:'}</Heading>
-            {currentWord}
-          </>
-          : 
-          <>
-            <Flex direction={'column'}>
-              <Input
-                placeholder='Type your guess here'
-                value={guess}
-                onChange={event => setGuess(event.target.value)}
-                key='guessInput'
-              />
-              <Button
-                onClick={async () => {
-                  try {
-                    await gameAreaController.makeGuess(guess).then(() => {
-                      setGuess('');
-                      if (gameAreaController.weAlreadyGuessedCorrectly) {
-                        toast({
-                          title: 'Correct!',
-                          status: 'success',
-                        });
-                      } else {
-                        toast({
-                          title: 'Incorrect.',
-                          status: 'error',
-                        });
-                      }
-                    });
-                  } catch (e) {
-                    toast({
-                      title: 'Error making guess',
-                      description: (e as Error).toString(),
-                      status: 'error',
-                    });
-                  }
-                }}>
-                Guess
-              </Button>
-            </Flex>
-          </>
+          isPlayer 
+          ?
+            // If is player, display info based on role/game phase
+            isOurTurn || betweenTurns
+            ? 
+            // Show current role to drawer/if we are in an intermission
+            currentWordDisplay
+            : 
+            // Show guess field to guessers
+            <>
+              <Flex direction={'column'}>
+                <Input
+                  placeholder='Type your guess here'
+                  value={guess}
+                  onChange={event => setGuess(event.target.value)}
+                  key='guessInput'
+                />
+                <Button
+                  onClick={async () => {
+                    try {
+                      await gameAreaController.makeGuess(guess).then(() => {
+                        setGuess('');
+                        if (gameAreaController.weAlreadyGuessedCorrectly) {
+                          toast({
+                            title: 'Correct!',
+                            status: 'success',
+                          });
+                        } else {
+                          toast({
+                            title: 'Incorrect.',
+                            status: 'error',
+                          });
+                        }
+                      });
+                    } catch (e) {
+                      toast({
+                        title: 'Error making guess',
+                        description: (e as Error).toString(),
+                        status: 'error',
+                      });
+                    }
+                  }}>
+                  Guess
+                </Button>
+              </Flex>
+            </>
+            :
+            // Do not show guess box to observers. Only show the current word during intermission
+            betweenTurns
+            ?
+            currentWordDisplay
+            :
+            <></>
         }
         <EndGameScore scores={gameAreaController.scores}/>
       </VStack>
